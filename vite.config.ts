090d61c3dcedf37865eadcf780a3198c275b4dae
Venkata Sai Showri Konda <showrirock@gmail.com>
2025-08-29 13:20:41 -0400

    workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/Volleyball-Manager/', // Add this line
+    define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.SUPABASE_URL': JSON.stringify(env.SUPABASE_URL),
